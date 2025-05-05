77846f1fa0352f0828055e66dc8e7fe04b92a151
papon <bkknm121@gmail.com>
2025-05-06 04:34:02 +0700

    ss2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -203,7 +203,7 @@ export default function Page() {
                 Contact
               </div>
               <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                Get in Touch sss
+                Get in Touch
               </h2>
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                 Want to chat? Just shoot me a dm{" "}
